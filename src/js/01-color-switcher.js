df195a965577ed8d4f6f722f9b890aec7606ab83
anetta999 <124580023+anetta999@users.noreply.github.com>
2023-07-06 11:57:32 +0300

    01-color-switcher.js is completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+console.log(startBtn);
+console.log(stopBtn);
+let timerId;
 
+startBtn.addEventListener('click', changeBodyBackgroundColor);
+function changeBodyBackgroundColor(evt) {
+  if (evt.target !== evt.currentTarget) {
+    return;
+  }
+
+  timerID = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }, 1000);
+}
+
+stopBtn.addEventListener('click', stopChangingColor);
+function stopChangingColor(evt) {
+  if (evt.target !== evt.currentTarget) {
+    return;
+  }
+
+  clearInterval(timerID);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
